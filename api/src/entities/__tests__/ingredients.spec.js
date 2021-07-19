84a5050ecb0b8bcf2fd790e77ae8131cab729cc5
Alexander Einerskog <77435792+Wppvater@users.noreply.github.com>
2021-07-19 14:31:41 +0200

    Ingredient entity complete with tests
    Co-authored-by: philipdaveby <philip.daveby@gmail.com>
    Co-authored-by: jiayuwaern <jiayuwaern@gmail.com>

--- api/src/entities/__tests__/ingredients.spec.js
+++ api/src/entities/__tests__/ingredients.spec.js
@@ -24,10 +24,117 @@ const fakeIngredient = {
   }]
 }
 describe('ingredient entity', () =>{
+  let testIngredient = {};
+  beforeEach( () => {
+    testIngredient = {
+      id: fakeIngredient.id,
+      name: fakeIngredient.name,
+      group: fakeIngredient.group,
+      energy: fakeIngredient.energy,
+      carbohydrates: fakeIngredient.carbohydrates,
+      protein: fakeIngredient.protein,
+      fat: fakeIngredient.fat,
+      microNutrients: [...fakeIngredient.microNutrients],
+    }
+  })
   test('is exported correctly', () =>{
     expect(typeof makeIngredient).toBe('function');
   });
   test('return an object', () => {
     expect(typeof makeIngredient(fakeIngredient)).toBe('object');
+  })
+  test('throws an error if no ID is provided', () =>{
+    delete testIngredient.id;
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have id.');
+  });
+  test('throws an error if no name is provided', () =>{
+    delete testIngredient.name;
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have name.');
+  });
+  test('throws an error if no group is provided', () =>{
+    delete testIngredient.group;
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have group.');
+  });
+  test('throws an error if no energy is provided', () =>{
+    delete testIngredient.energy;
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have energy.');
+  });
+  test('throws an error if no carbohydrates is provided', () =>{
+    delete testIngredient.carbohydrates;
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have carbohydrates.');
+  });
+  test('throws an error if no protein is provided', () =>{
+    delete testIngredient.protein;
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have protein.');
+  });
+  test('throws an error if no fat is provided', () => {
+    delete testIngredient.fat;
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have fat.');
+  });
+  test('throws an error if no microNutrients is provided', () =>{
+    delete testIngredient.microNutrients;
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have microNutrients.');
+  });
+  test('throws an error if energy is not a positive number', () =>{
+    testIngredient.energy = '5htr';
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have a positive number of energy.');
+    testIngredient.energy = -1;
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have a positive number of energy.');
+    testIngredient.energy = 0;
+    expect(() => makeIngredient(testIngredient)).not.toThrow();
+  });
+  test('throws an error if protein is not a positive number', () =>{
+    testIngredient.protein = '5htr';
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have a positive number of protein.');
+    testIngredient.protein = -1;
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have a positive number of protein.');
+    testIngredient.protein = 0;
+    expect(() => makeIngredient(testIngredient)).not.toThrow();
+  });
+  test('throws an error if carbohydrates is not a positive number', () =>{
+    testIngredient.carbohydrates = '5htr';
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have a positive number of carbohydrates.');
+    testIngredient.carbohydrates = -1;
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have a positive number of carbohydrates.');
+    testIngredient.carbohydrates = 0;
+    expect(() => makeIngredient(testIngredient)).not.toThrow();
+  });
+  test('throws an error if fat is not a positive number', () =>{
+    testIngredient.fat = '5htr';
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have a positive number of fat.');
+    testIngredient.fat = -1;
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have a positive number of fat.');
+    testIngredient.fat = 0;
+    expect(() => makeIngredient(testIngredient)).not.toThrow();
+  });
+  test('throws an error if microNutrients is an empty array', () =>{
+    testIngredient.microNutrients = [];
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have a microNutrients array.');
+    testIngredient.microNutrients = '[]';
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient must have a microNutrients array.');
+  });
+  test('throws an error if microNutrients information are not in correct format', () =>{
+    testIngredient.microNutrients.push({name: 1, amount: 1, unit: "grams"});
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient\'s microNutrients name must be a string.');
+    testIngredient.microNutrients.pop();
+    testIngredient.microNutrients.push({name: "hi", amount: 'jsja8', unit: "grams"});
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient\'s microNutrients amount must be a number.');
+    testIngredient.microNutrients.pop();
+    testIngredient.microNutrients.push({name: "hi", amount: 1, unit: "djeush"});
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient\'s microNutrients unit must be grams, micro and milli.');
+    testIngredient.microNutrients.pop();
+    testIngredient.microNutrients.push({name: "hi", amount: 1, unit: 2});
+    expect(() => makeIngredient(testIngredient)).toThrow('Ingredient\'s microNutrients unit must be a string.');
+  });
+  test('returns an object', () => {
+    const returnedIngredient = makeIngredient(fakeIngredient);
+    expect(returnedIngredient.getId()).toBe(fakeIngredient.id);
+    expect(returnedIngredient.getName()).toBe(fakeIngredient.name);
+    expect(returnedIngredient.getGroup()).toBe(fakeIngredient.group);
+    expect(returnedIngredient.getEnergy()).toBe(fakeIngredient.energy);
+    expect(returnedIngredient.getCarbohydrates()).toBe(fakeIngredient.carbohydrates);
+    expect(returnedIngredient.getFat()).toBe(fakeIngredient.fat);
+    expect(returnedIngredient.getProtein()).toBe(fakeIngredient.protein);
+    expect(returnedIngredient.getMicroNutrients()).toEqual(fakeIngredient.microNutrients);
   })
 });
--- api/src/entities/ingredients.js
+++ api/src/entities/ingredients.js
@@ -1,34 +1,85 @@
 const buildMakeIngredient = () => {
   return ({
     id,
-    productId,
-    productName,
-    productGroup,
-    title,
-    description,
-    reviewer,
-    upvotes,
-    numberOfUpvotes,
+    name,
+    group,
+    energy,
+    carbohydrates,
+    protein,
+    fat,
+    microNutrients,
   } = {}) => {
-    // if (!productId) {
-    //   console.log('Review must have a productId'); // This should be an error in reality
-    // }
-    // if (!description) {
-    //   console.log('Review must have text in description'); // This should be an error in reality
-    // }
-    // if (upvotes.length !== numberOfUpvotes) {
-    //   console.log('numberOfUpvotes must be the same as the length of upvotes');
-    // }
-    return Object.freeze({
+    if (!id) {
+      throw new Error('Ingredient must have id.'); 
+    }
+    if (!name) {
+      throw new Error('Ingredient must have name.'); 
+    }
+    if (!group) {
+      throw new Error('Ingredient must have group.');
+    }
+    if (energy === undefined) {
+      throw new Error('Ingredient must have energy.'); 
+    }
+    if (typeof energy !== 'number' || energy < 0) {
+      throw new Error('Ingredient must have a positive number of energy.')
+    }
+    if (carbohydrates === undefined) {
+      throw new Error('Ingredient must have carbohydrates.');
+    }
+    if (typeof carbohydrates !== 'number' || carbohydrates < 0) {
+      throw new Error('Ingredient must have a positive number of carbohydrates.')
+    }
+    if (protein === undefined) {
+      throw new Error('Ingredient must have protein.'); 
+    }
+    if (typeof protein !== 'number' || protein < 0) {
+      throw new Error('Ingredient must have a positive number of protein.')
+    }
+    if (fat === undefined) {
+      throw new Error('Ingredient must have fat.'); 
+    }
+    if (typeof fat !== 'number' || fat < 0) {
+      throw new Error('Ingredient must have a positive number of fat.')
+    }
+    if (microNutrients === undefined) {
+      throw new Error('Ingredient must have microNutrients.'); 
+    }
+    if(typeof microNutrients !== 'object' || microNutrients.length == 0){
+      throw new Error('Ingredient must have a microNutrients array.'); 
+    }
+    microNutrients.forEach(microNutrient => {
+      if(!microNutrient.name){
+        throw new Error('Ingredient\'s microNutrients must have name.')
+      }
+      if(!microNutrient.unit){
+        throw new Error('Ingredient\'s microNutrients must have unit.')
+      }
+      if(microNutrient.amount == undefined){
+        throw new Error('Ingredient\'s microNutrients must have amount.')
+      }
+      if(typeof microNutrient.name !== 'string'){
+        throw new Error('Ingredient\'s microNutrients name must be a string.')
+      }
+      if(typeof microNutrient.amount !== 'number'){
+        throw new Error('Ingredient\'s microNutrients amount must be a number.')
+      }
+      if(typeof microNutrient.unit !== 'string'){
+        throw new Error('Ingredient\'s microNutrients unit must be a string.')
+      }
+      if(microNutrient.unit !== 'grams' && microNutrient.unit !== 'milli' && microNutrient.unit !== 'micro'){
+        throw new Error('Ingredient\'s microNutrients unit must be grams, micro and milli.')
+      }
+    });
+    return Object.freeze({  
       getId: () => id,
-      getProductId: () => productId,
-      getProductName: () => productName,
-      getProductGroup: () => productGroup,
-      getTitle: () => title,
-      getDescription: () => description,
-      getReviewer: () => reviewer,
-      getUpvotes: () => upvotes,
-      getNumberOfUpvotes: () => numberOfUpvotes,
+      getName: () => name,
+      getGroup: () => group,
+      getEnergy: () => energy,
+      getCarbohydrates: () => carbohydrates,
+      getProtein: () => protein,
+      getFat: () => fat,
+      getMicroNutrients: () => microNutrients,
     });
   };
 };
