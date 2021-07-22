65bb6eaee7fbef99a85e49e2fb3e6ff5dbae3e06
Alexander Einerskog <77435792+Wppvater@users.noreply.github.com>
2021-07-22 11:04:45 +0200

    Configured graphql connection to display data on webpage.
    Co-authored-by: philipdaveby <philip.daveby@gmail.com>
    Co-authored-by: jiayuwaern <jiayuwaern@gmail.com>

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -3,5 +3,19 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "client",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: ["gatsby-plugin-sass", 
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: "SaltedPAJ",
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: "saltedpaj",
+        // Url to query from
+        url: "http://localhost:3000/api/",
+      },
+    },
+  ],
+    // Simple config, passing URL
+    
 };
