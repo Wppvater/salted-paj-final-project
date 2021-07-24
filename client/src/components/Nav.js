725208a4d73a5f5eda2d694a7b2289169cf651dc
philipdaveby <philip.daveby@gmail.com>
2021-07-24 16:43:41 +0200

    fbetter looking header and footer

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -8,24 +8,16 @@ import profileIcon from '../images/profile.png';
 const Nav = () => (
   <nav className="nav">
     <Link to="/">
-      <div className="nav__home">
-      <img src={homeIcon} alt="home-icon" className="nav__icon" />
-      </div>
+      <img src={homeIcon} alt="home-icon" className="nav__icon nav__home" />
     </Link>
     <Link to="/plan">
-      <div className="nav__plan">
-      <img src={planIcon} alt="plan-icon" className="nav__icon" />
-      </div>
+      <img src={planIcon} alt="plan-icon" className="nav__icon nav__plan nav__icon__inactive" />
     </Link>
     <Link to="/recipes">
-      <div className="nav__recipes">
-      <img src={recipeIcon} alt="recipe-icon" className="nav__icon" />
-      </div>
+      <img src={recipeIcon} alt="recipe-icon" className="nav__icon nav__recipes nav__icon__inactive" />
     </Link>
     <Link to="/profile">
-      <div className="nav_profile">
-      <img src={profileIcon} alt="profile-icon" className="nav__icon" />
-      </div>
+      <img src={profileIcon} alt="profile-icon" className="nav__icon nav__profile nav__icon__inactive" />
     </Link>
   </nav>
   );
